f24d23f83a66d3d7441fc2b89df305969d2e5aef
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-06-12 09:43:06 +0530

    password generator done

--- PasswordGen/src/App.jsx
+++ PasswordGen/src/App.jsx
@@ -1,6 +1,37 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 function App() {
   const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const passRef = useRef(null);
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm";
+    if (numberAllowed) {
+      str += "123456789";
+    }
+    if (charAllowed) {
+      str += "!@#$%^&*-_+=[]{}~`";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, passwordGenerator, numberAllowed, charAllowed]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-center text-2xl text-black">Password Generator</h1>
@@ -11,8 +42,13 @@ function App() {
             type="text"
             placeholder="Password"
             readOnly
+            value={password}
+            ref={passRef}
           />
-          <button className="w-[20%] bg-blue-400 py-1 text-xl hover:bg-blue-500 select-none rounded-r-lg">
+          <button
+            className="w-[20%] bg-blue-400 py-1 text-xl hover:bg-blue-500 select-none rounded-r-lg"
+            onClick={copyPasswordToClipboard}
+          >
             Copy
           </button>
         </div>
@@ -22,7 +58,7 @@ function App() {
             className="cursor-pointer"
             id="one"
             min={8}
-            max={100}
+            max={30}
             value={length}
             onChange={(e) => {
               setLength(e.target.value);
@@ -30,12 +66,32 @@ function App() {
           />
           <label htmlFor="one">Length : {length}</label>
           <div>
-            <input type="checkbox" id="numberInput" />
-            <label className="ml-1" htmlFor="numberInput">Numbers</label>
+            <input
+              type="checkbox"
+              id="numberInput"
+              defaultChecked={numberAllowed}
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label className="ml-1 select-none cursor-pointer" htmlFor="numberInput">
+              Numbers
+            </label>
           </div>
           <div>
-            <input type="checkbox" id="charInput" />
-            <label className="ml-1" htmlFor="charInput">Character</label>
+            <input
+              type="checkbox"
+              id="charInput"
+              defaultChecked={charAllowed}
+              onChange={() => {
+                setCharAllowed((prev) => {
+                  return !prev;
+                });
+              }}
+            />
+            <label className="ml-1 select-none cursor-pointer" htmlFor="charInput">
+              Character
+            </label>
           </div>
         </div>
       </div>
